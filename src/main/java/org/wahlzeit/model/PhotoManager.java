92831d7c3644489a71d1157fc5da39cb0b467b54
Matthias Altstadt <matthias.altstadt@fau.de>
2015-11-02 12:56:54 +0100

    Hook up CraftbeerPhotoFactory into the framework

--- src/main/java/org/wahlzeit/model/PhotoManager.java
+++ src/main/java/org/wahlzeit/model/PhotoManager.java
@@ -66,7 +66,7 @@ public class PhotoManager extends ObjectManager {
 	 *
 	 */
 	public PhotoManager() {
-		photoTagCollector = PhotoFactory.getInstance().createPhotoTagCollector();
+		photoTagCollector = CraftbeerPhotoFactory.getInstance().createPhotoTagCollector();
 	}
 
 	/**
@@ -108,7 +108,7 @@ public class PhotoManager extends ObjectManager {
 		Photo result = doGetPhotoFromId(id);
 
 		if (result == null) {
-			result = PhotoFactory.getInstance().loadPhoto(id);
+			result = CraftbeerPhotoFactory.getInstance().loadPhoto(id);
 			if (result != null) {
 				doAddPhoto(result);
 			}
